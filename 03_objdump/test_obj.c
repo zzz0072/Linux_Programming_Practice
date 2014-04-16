c660ee215c0899464134d70a1041a578ad94338c
Wen.Liao <zzz00072@gmail.com>
2014-04-16 11:09:18 +0800

    Check/list section symbols in 03_objdump

--- 03_objdump/test_obj.c
+++ 03_objdump/test_obj.c
@@ -3,7 +3,10 @@
 
 #define MAX_CHARS (32)
 
-extern int _binary_my_data_txt_start;
+extern char _binary_my_data_txt_start;
+extern char _binary_my_data_txt_end;
+extern int _binary_my_data_txt_size;
+
 int main(int argc, char **argv)
 {
     char line[MAX_CHARS] = {0};
@@ -16,6 +19,9 @@ int main(int argc, char **argv)
 
     strncpy(line, my_data_start_addr + 1, str_len);
 
+    printf("_binary_my_data_txt_start: %c\n", _binary_my_data_txt_start);
+    printf("_binary_my_data_txt_end: %c\n", _binary_my_data_txt_end);
+    printf("_binary_my_data_txt_size: %p\n", &_binary_my_data_txt_size);
     printf("Read string is %s\n", line);
 
     return 0;
