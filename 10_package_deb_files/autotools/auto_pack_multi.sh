44c009af240c84debcaca9f67567116aef25c628
Wen.Liao <zzz00072@gmail.com>
2014-06-27 15:38:17 +0800

    10_package_deb_files: Build multi-packages for a single tarball

---
+++ 10_package_deb_files/autotools/auto_pack_multi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+# Prepare tarball
+SRC_DIR=../../08_autoconf
+cd $SRC_DIR
+./autogen.sh
+./configure
+make dist
+
+# Pack package with --createorig option
+cd -
+TEST_DIR=test
+rm -rf $TEST_DIR
+mkdir -p $TEST_DIR
+cd $TEST_DIR
+tar -zxvf ../$SRC_DIR/testautotools-0.tar.gz
+cd testautotools-0
+echo -e "\n" | dh_make --createorig -s
+cp ../../debian/* ./debian
+dpkg-buildpackage -uc -us
