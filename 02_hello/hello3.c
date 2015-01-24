f66ef9879cac90a67c4ebfee38364d0a0720d944
Wen.Liao <zzz00072@gmail.com>
2015-01-24 14:19:14 +0800

    02_hello: Add 4 hello world version.

---
+++ 02_hello/hello3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "Hello world.\n";
+	printf("%s", str);
+
+	return 0;
+}
---
+++ 02_hello/hello4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	static char str[] = "Hello world.\n";
+	printf("%s", str);
+
+	return 0;
+}
